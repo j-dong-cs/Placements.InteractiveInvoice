672b4844eeb6b843045eb8b336be56b2d28be6b5
j-dong-cs <jingjing.dong.mil@outlook.com>
2020-01-16 00:03:56 -0800

    * Campaign.cs: Create models.
    * User.cs:
    * Comment.cs:
    * Invoice.cs:
    * LineItem.cs:

---
+++ Placement.InteractiveInvoice/Models/LineItem.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Placement.InteractiveInvoice.Models
+{
+    public class LineItem
+    {
+        [JsonPropertyName("id")]
+        public int LineItemID { get; set; }
+
+        [JsonPropertyName("campaign_id")]
+        public int CampaignID { get; set; }
+
+        [JsonPropertyName("campaign_name")]
+        public string CampaignName { get; set; }
+
+        [Required]
+        [JsonPropertyName("line_item_name")]
+        public string LineItemName { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        [JsonPropertyName("booked_amount")]
+        public Decimal BookedAmount { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        [JsonPropertyName("actual_amount")]
+        public Decimal ActualAmount { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        [JsonPropertyName("adjustments")]
+        public Decimal Adjustments { get; set; }
+
+        // one lineitem only belong to one campaign
+        public virtual Campaign Campaign { get; set; }
+
+        // one lineitem has many comments
+        public virtual ICollection<Comment> Comments { get; set; }
+
+        // one lineitem exist in many invoices
+        public virtual ICollection<Invoice> Invoices { get; set; }
+    }
+}
---
+++ Placement.InteractiveInvoice/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Placement.InteractiveInvoice.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        // one user can create many comments
+        public virtual ICollection<Comment> Comments { get; set; }
+
+        // one user can create many invoices
+        public virtual ICollection<Invoice> Invoices { get; set; }
+    }
+}
